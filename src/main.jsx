6b46c4b0d9be5d8c2024ebd11808dc41a99b3753
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-15 16:45:45 -0500

    useRef Hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { MultipleCustomHooks } from './03-example/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 import { HooksApp } from './HooksApp'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { CounterApp } from './01-useState/CounterApp'
@@ -18,7 +19,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* USE EFFECT */}
     {/* <SimpleForm /> */}
     {/* <FormWithCustomHook /> */}
-    <MultipleCustomHooks />
+    {/* <MultipleCustomHooks /> */}
+    <FocusScreen />
 
   </React.StrictMode>
 )
