b8b8c59110cb9ff4692aca7c2d4861491aff8440
Israel Trujillo Domínguez <ingisraeltrujillo@gmail.com>
2022-09-16 09:44:14 -0500

    Resolución Tarea

--- src/07-tarea-memo/Padre.jsx
+++ src/07-tarea-memo/Padre.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { Hijo } from './Hijo'
 
 export const Padre = () => {
   const numeros = [2, 4, 6, 8, 10]
   const [valor, setValor] = useState(0)
 
-  const incrementar = (num) => {
-    setValor(valor + num)
-  }
+  const incrementar = useCallback(
+    (num) => {
+      setValor((valor) => valor + num)
+    },
+    []
+  )
 
   return (
     <div>
@@ -17,14 +20,14 @@ export const Padre = () => {
       <hr />
 
       {
-                numeros.map(n => (
-                  <Hijo
-                    key={n}
-                    numero={n}
-                    incrementar={incrementar}
-                  />
-                ))
-            }
+        numeros.map(n => (
+          <Hijo
+            key={n}
+            numero={n}
+            incrementar={incrementar}
+          />
+        ))
+      }
       {/* <Hijo /> */}
     </div>
   )
